077d981d8981d38595ce5491f8a8826e30be301c
Varmo <101868197+varmoh@users.noreply.github.com>
2024-07-29 09:02:47 +0300

    Update opensearch.js

--- pipelines/opensearch.js
+++ pipelines/opensearch.js
@@ -6,7 +6,9 @@ import YAML from "yaml";
 import sanitize from "string-sanitizer";
 import { Client } from "@opensearch-project/opensearch";
 import setRateLimit from "express-rate-limit";
-import path from 'path';
+import path from "path";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 const router = express.Router();
 const upload = multer({ 
@@ -14,7 +16,7 @@ const upload = multer({
   limits: { 
     fileSize: 50 * 1000 * 1000
   },
- });
+})
 
 const rateLimit = setRateLimit({
   windowMs: 60 * 1000,
@@ -22,12 +24,13 @@ const rateLimit = setRateLimit({
   message: "Too many requests",
   headers: true,
   statusCode: 429,
-});
+})
 
 const sanitizeFilename = (filename) => {
   if(filename.includes('../')) {
     throw new Error('relative paths are not allowed')
   }
+  
   return filename.replace('..', '');
 }
 
@@ -53,6 +56,7 @@ export async function osPut(index_name, document) {
     body: document,
     refresh: true,
   });
+  console.log('osPut response:', response);
   return response;
 }
 
@@ -61,6 +65,7 @@ export async function osDeleteIndex(index_name) {
   const response = await client.indices.delete({
     index: index_name,
   });
+  console.log('osDeleteIndex response:', response);
   return response;
 }
 
@@ -70,6 +75,7 @@ export async function osDeleteObject(index_name, obj_id) {
     index: index_name,
     id: obj_id,
   });
+  console.log('Deleted object with ID:', obj_id);
 }
 
 function getInput(req) {
@@ -81,51 +87,51 @@ function getInput(req) {
   }
 }
 
-// Create a write stream (in append mode) for logging
-const logStream = fs.createWriteStream(path.join(path.dirname(new URL(import.meta.url).pathname), '../app.log'), { flags: 'a' });
-
-const logRequest = (message) => {
-  console.log(message);
-  logStream.write(`${message}\n`);
-};
-
 /*
   For intents, one entity per file
 */
-router.post("/put/:index_name/:index_type", upload.single("input"), rateLimit, (req, res) => {
-  const input = getInput(req);
+router.post(
+  "/put/:index_name/:index_type",
+  upload.single("input"),
+  rateLimit,
+  (req, res) => {
+    let input = getInput(req);
 
-  logRequest(`Received PUT request for /put/${req.params.index_name}/${req.params.index_type} with input: ${JSON.stringify(input)}`);
+    if (input.nlu) input = input.nlu;
 
-  if (input.nlu) input = input.nlu;
-  if (input.data) input = input.data;
+    if (input.data) input = input.data;
 
-  const index_name = req.params.index_name;
-  const index_type = req.params.index_type;
-  const obj = input[0];
+    const index_name = req.params.index_name;
+    const index_type = req.params.index_type;
 
-  if (index_type) {
-    obj.id = obj[index_type].replace(/\s+/g, "_");
+    const obj = input[0];
+
+    // Check if index_type is defined and obj[index_type] exists
+    if (index_type && obj[index_type]) {
+      obj.id = obj[index_type].replaceAll(/\s+/g, "_");
+    } else {
+      return res.status(400).json({ error: "index_type is missing or invalid" });
+    }
+
+    osPut(index_name, obj)
+      .then((ret) => {
+        console.log('Put object:', obj);
+        res.status(200);
+        res.json(JSON.stringify(ret)).end();
+      })
+      .catch((e) => {
+        console.error(e);
+        res.status(500);
+        res.end();
+      });
   }
-
-  osPut(index_name, obj)
-    .then((ret) => {
-      logRequest(`PUT request successful for /put/${index_name}/${index_type} with response: ${JSON.stringify(ret)}`);
-      res.status(200).json(ret).end();
-    })
-    .catch((e) => {
-      logRequest(`Error in PUT request for /put/${index_name}/${index_type}: ${e.message}`);
-      res.status(500).json({ error: e.message }).end();
-    });
-});
+);
 
 /*
   For config and domain - many different types of entities in one list 
 */
 router.post("/bulk/:index_name", upload.single("input"), rateLimit, (req, res) => {
   const input = getInput(req);
-
-  logRequest(`Received BULK request for /bulk/${req.params.index_name} with input: ${JSON.stringify(input)}`);
 
   const index_name = req.params.index_name;
 
@@ -134,50 +140,55 @@ router.post("/bulk/:index_name", upload.single("input"), rateLimit, (req, res) =
     const inp = {};
     inp[key] = input[key];
     inp.id = key;
-    osPut(index_name, inp).catch(e => {
-      logRequest(`Error in BULK request for /bulk/${index_name} key ${key}: ${e.message}`);
-      console.error(e);
-    });
+    osPut(index_name, inp).catch(console.error);
   }
+  console.log('Bulk put completed for index:', index_name);
   res.end();
 });
 
 /*
-  For rules, regexes, and stories with one type of entities in a list
+  For rules, regexes and stories with one type of entities in a list
 */
-router.post("/bulk/:index_name/:index_type", upload.single("input"), rateLimit, (req, res) => {
-  const input = getInput(req);
+router.post(
+  "/bulk/:index_name/:index_type",
+  upload.single("input"),
+  rateLimit,
+  (req, res) => {
+    const input = getInput(req);
 
-  logRequest(`Received BULK request for /bulk/${req.params.index_name}/${req.params.index_type} with input: ${JSON.stringify(input)}`);
+    const index_name = req.params.index_name;
+    const index_type = req.params.index_type;
 
-  const index_name = req.params.index_name;
-  const index_type = req.params.index_type;
-
-  input[index_name].forEach((obj) => {
-    obj.id = sanitize.sanitize.addDash(obj[index_type]);
-    osPut(index_name, obj).catch((e) => {
-      logRequest(`Error in BULK request for /bulk/${index_name}/${index_type}: ${e.message}`);
-      res.status(500).end();
-      console.error(e);
+    input[index_name].forEach((obj) => {
+      if (obj[index_type]) {
+        obj.id = sanitize.sanitize.addDash(obj[index_type]);
+        osPut(index_name, obj).catch((e) => {
+          res.status(500);
+          res.end();
+          console.error(e);
+        });
+      } else {
+        console.error('Missing index_type for object:', obj);
+      }
     });
-  });
-  res.status(200).end();
-});
+    console.log('Bulk put completed for index:', index_name, 'with type:', index_type);
+    res.status(200);
+    res.end();
+  }
+);
 
 router.post("/delete/:index_name", (req, res) => {
   const index_name = req.params.index_name;
-
-  logRequest(`Received DELETE request for /delete/${index_name}`);
-
   osDeleteIndex(index_name)
     .then((ret) => {
-      logRequest(`DELETE request successful for /delete/${index_name} with response: ${JSON.stringify(ret)}`);
-      res.status(200).json(ret).end();
+      console.log('Deleted index:', index_name);
+      res.status(200);
+      res.json(JSON.stringify(ret)).end();
     })
     .catch((e) => {
-      logRequest(`Error in DELETE request for /delete/${index_name}: ${e.message}`);
-      res.status(500).end();
       console.error(e);
+      res.status(500);
+      res.end();
     });
 });
 
@@ -185,17 +196,16 @@ router.post("/delete/object/:index_name", (req, res) => {
   const index_name = req.params.index_name;
   const obj_id = req.body.id;
 
-  logRequest(`Received DELETE request for /delete/object/${index_name} with obj_id: ${obj_id}`);
-
   osDeleteObject(index_name, obj_id)
     .then((ret) => {
-      logRequest(`DELETE object request successful for /delete/object/${index_name} with response: ${JSON.stringify(ret)}`);
-      res.status(200).json(ret).end();
+      console.log('Deleted object with ID:', obj_id);
+      res.status(200);
+      res.json(JSON.stringify(ret)).end();
     })
     .catch((e) => {
-      logRequest(`Error in DELETE object request for /delete/object/${index_name}: ${e.message}`);
-      res.status(500).end();
       console.error(e);
+      res.status(500);
+      res.end();
     });
 });
 
@@ -203,17 +213,16 @@ router.post("/delete/:index_name/:obj_id", (req, res) => {
   const index_name = req.params.index_name;
   const obj_id = req.params.obj_id;
 
-  logRequest(`Received DELETE request for /delete/${index_name}/${obj_id}`);
-
   osDeleteObject(index_name, obj_id)
     .then((ret) => {
-      logRequest(`DELETE request successful for /delete/${index_name}/${obj_id} with response: ${JSON.stringify(ret)}`);
-      res.status(200).json(ret).end();
+      console.log('Deleted object with ID:', obj_id, 'from index:', index_name);
+      res.status(200);
+      res.json(JSON.stringify(ret)).end();
     })
     .catch((e) => {
-      logRequest(`Error in DELETE request for /delete/${index_name}/${obj_id}: ${e.message}`);
-      res.status(500).end();
       console.error(e);
+      res.status(500);
+      res.end();
     });
 });
 
